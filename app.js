fc9cfe681e051d5f7abe582741b1fbc5e92abd72
NQTruong02 <nguyentruongbg2002@gmail.com>
2023-06-16 21:06:28 +0700

    aaaaaaaaaaaa

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ app.use(function(err, req, res, next) {
 });
 
 //setup port for deployment
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log("Server is running at http://localhost:3001");
+  console.log("Server is running at http://localhost:10000");
 });
 module.exports = app;
